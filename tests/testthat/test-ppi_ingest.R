e6141559aa94e4f19981db87591a982c2e9cf43c
Dave Weaver <dweav94@gmail.com>
2021-08-12 20:35:43 -0400

    I swear this one will work

--- tests/testthat/test-ppi_ingest.R
+++ tests/testthat/test-ppi_ingest.R
@@ -1,10 +1,10 @@
 
 test_that("prep_stringdb returns an igraph", {
-  skip_on_ci("don't test these during CI on github")
+  skip_on_ci()
   expect_true(igraph::is.igraph(prep_stringdb()))
 })
 test_that("prep_biogrid returns an igraph", {
-  skip_on_ci("don't test these during CI on github")
+  skip_on_ci()
   expect_true(igraph::is.igraph(prep_biogrid()))
 })
 
